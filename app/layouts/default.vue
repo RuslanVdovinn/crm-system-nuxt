c5b0c0d51d46a57200302eb459c0ffe7372d65e4
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-11-27 12:54:09 +0700

    layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang='ts' setup>
+console.log('Loaded')
+</script>
+
+
+<template>
+    <section>
+        <div>
+            <slot />
+        </div>
+    </section>
+
+</template>
+
+<style scoped></style>
